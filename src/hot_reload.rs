5b3a723108e0b8714eb3139c59fcf6ceea30b21f
timtom-dev <timtom.business@gmail.com>
2021-08-19 01:57:25 -0700

    remove unneeded attributes

--- src/hot_reload.rs
+++ src/hot_reload.rs
@@ -1,31 +1,26 @@
 use crate::tree::{A, NODE_COUNT};
 
 #[no_mangle]
-#[cfg(all(debug_assertions, feature = "hot-reload"))]
 pub fn _rosin_reset_alloc() -> Result<(), ()> {
     A.with(|a| a.reset())
 }
 
 #[no_mangle]
-#[cfg(all(debug_assertions, feature = "hot-reload"))]
 pub fn _rosin_begin_alloc() {
     A.with(|a| a.begin())
 }
 
 #[no_mangle]
-#[cfg(all(debug_assertions, feature = "hot-reload"))]
 pub fn _rosin_end_alloc() {
     A.with(|a| a.end())
 }
 
 #[no_mangle]
-#[cfg(all(debug_assertions, feature = "hot-reload"))]
 pub fn _rosin_reset_node_count() {
     NODE_COUNT.with(|c| c.set(0))
 }
 
 #[no_mangle]
-#[cfg(all(debug_assertions, feature = "hot-reload"))]
 pub fn _rosin_get_node_count() -> usize {
     NODE_COUNT.with(|c| c.get())
 }
