ace15351ceaa6e174c876636854597eec5424ab0
timtom-dev <timtom.business@gmail.com>
2021-12-29 00:16:49 -0800

    remove associated types from lenses

--- src/lenses.rs
+++ src/lenses.rs
@@ -3,6 +3,7 @@
 use std::{
     any, fmt,
     fmt::{Debug, Display},
+    marker::PhantomData,
 };
 
 /// Create a new Lens.
@@ -17,13 +18,10 @@ macro_rules! lens {
 }
 
 // ---------- Trait ----------
-/// A datatype that returns a reference to an internal component of another type. Used by widgets.
-pub trait Lens: Debug {
-    type In;
-    type Out;
-
-    fn get_ref<'a>(&self, obj: &'a Self::In) -> &'a Self::Out;
-    fn get_mut<'a>(&self, obj: &'a mut Self::In) -> &'a mut Self::Out;
+/// A datatype that returns a reference to an internal component of another type. Intended to be used by widgets.
+pub trait Lens<A, B>: Debug {
+    fn get_ref<'a>(&self, obj: &'a A) -> &'a B;
+    fn get_mut<'a>(&self, obj: &'a mut A) -> &'a mut B;
 }
 
 // ---------- SingleLens ----------
@@ -58,10 +56,7 @@ impl<A, B> Display for SingleLens<A, B> {
     }
 }
 
-impl<A, B> Lens for SingleLens<A, B> {
-    type In = A;
-    type Out = B;
-
+impl<A, B> Lens<A, B> for SingleLens<A, B> {
     fn get_ref<'a>(&self, obj: &'a A) -> &'a B {
         (self.get_ref)(obj)
     }
@@ -73,62 +68,68 @@ impl<A, B> Lens for SingleLens<A, B> {
 
 // ---------- CompoundLens ----------
 #[doc(hidden)]
-pub struct CompoundLens<X, Y> {
+pub struct CompoundLens<A, B, C, X, Y> {
     pub lhs: X,
     pub rhs: Y,
+    _a: PhantomData<A>,
+    _b: PhantomData<B>,
+    _c: PhantomData<C>,
 }
 
-impl<X, Y> CompoundLens<X, Y>
+impl<A, B, C, X, Y> CompoundLens<A, B, C, X, Y>
 where
-    X: Lens,
-    Y: Lens,
+    X: Lens<A, B>,
+    Y: Lens<B, C>,
 {
     pub fn new(lhs: X, rhs: Y) -> Self {
-        CompoundLens { lhs, rhs }
+        CompoundLens {
+            lhs,
+            rhs,
+            _a: PhantomData,
+            _b: PhantomData,
+            _c: PhantomData,
+        }
     }
 }
 
-impl<X: Copy, Y: Copy> Copy for CompoundLens<X, Y> {}
-impl<X: Copy, Y: Copy> Clone for CompoundLens<X, Y> {
+impl<A, B, C, X: Copy, Y: Copy> Copy for CompoundLens<A, B, C, X, Y> {}
+impl<A, B, C, X: Copy, Y: Copy> Clone for CompoundLens<A, B, C, X, Y> {
     fn clone(&self) -> Self {
         *self
     }
 }
 
-impl<'a, X, Y> Debug for CompoundLens<X, Y>
+impl<A, B, C, X, Y> Debug for CompoundLens<A, B, C, X, Y>
 where
-    X: Lens + Display,
-    Y: Lens + Display,
+    X: Lens<A, B> + Display,
+    Y: Lens<B, C> + Display,
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Lens({} => {} => {})", self.lhs, self.rhs, any::type_name::<Y::Out>())
+        write!(f, "Lens({} => {} => {})", self.lhs, self.rhs, any::type_name::<C>())
     }
 }
 
-impl<X, Y> Display for CompoundLens<X, Y>
+impl<A, B, C, X, Y> Display for CompoundLens<A, B, C, X, Y>
 where
-    X: Lens,
-    Y: Lens,
+    X: Lens<A, B>,
+    Y: Lens<B, C>,
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{} => {}", any::type_name::<X::In>(), any::type_name::<X::Out>())
+        write!(f, "{} => {}", any::type_name::<A>(), any::type_name::<B>())
     }
 }
 
-impl<A, B, C, X, Y> Lens for CompoundLens<X, Y>
+impl<A, B, C, X, Y> Lens<A, C> for CompoundLens<A, B, C, X, Y>
 where
-    X: 'static + Lens<In = A, Out = B> + Debug + Display,
-    Y: 'static + Lens<In = B, Out = C> + Debug + Display,
+    X: Lens<A, B> + Debug + Display,
+    Y: Lens<B, C> + Debug + Display,
     B: 'static,
 {
-    type In = X::In;
-    type Out = Y::Out;
-
-    fn get_ref<'a>(&self, obj: &'a Self::In) -> &'a Self::Out {
+    fn get_ref<'a>(&self, obj: &'a A) -> &'a C {
         self.rhs.get_ref(self.lhs.get_ref(obj))
     }
 
-    fn get_mut<'a>(&self, obj: &'a mut Self::In) -> &'a mut Self::Out {
+    fn get_mut<'a>(&self, obj: &'a mut A) -> &'a mut C {
         self.rhs.get_mut(self.lhs.get_mut(obj))
     }
 }
