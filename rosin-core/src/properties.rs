8d2e688f5b2348467ff7de9455dfedd4b2f8b41b
timtom-dev <timtom.business@gmail.com>
2022-03-25 02:24:23 -0700

    shortened apply_properties()

--- rosin-core/src/properties.rs
+++ rosin-core/src/properties.rs
@@ -98,7 +98,7 @@ macro_rules! apply {
                 }
             },
             _ => debug_assert!(false),
-        };
+        }
     };
     (@generic, $value:expr, $style:expr, $par_style:ident, $attr:ident) => {
         match $value {
@@ -114,7 +114,7 @@ macro_rules! apply {
                 $style.$attr = *value;
             }
             _ => debug_assert!(false),
-        };
+        }
     };
     (@generic_opt, $value:expr, $style:expr, $par_style:ident, $attr:ident) => {
         match $value {
@@ -130,7 +130,7 @@ macro_rules! apply {
                 $style.$attr = Some(*value);
             }
             _ => debug_assert!(false),
-        };
+        }
     };
     (@length, $value:expr, $style:expr, $par_style:ident, $attr:ident) => {
         match $value {
@@ -151,7 +151,7 @@ macro_rules! apply {
                 }
             },
             _ => debug_assert!(false),
-        };
+        }
     };
     (@length_opt, $value:expr, $style:expr, $par_style:ident, $attr:ident) => {
         match $value {
@@ -172,7 +172,7 @@ macro_rules! apply {
                     $style.$attr = Some(*value);
                 }
             },
-        };
+        }
     };
     (@length_max, $value:expr, $style:expr, $par_style:ident, $attr:ident) => {
         match $value {
@@ -193,7 +193,7 @@ macro_rules! apply {
                     $style.$attr = *value;
                 }
             },
-        };
+        }
     };
     (@length_min, $value:expr, $style:expr, $par_style:ident, $attr:ident) => {
         match $value {
@@ -214,7 +214,7 @@ macro_rules! apply {
                     $style.$attr = *value;
                 }
             },
-        };
+        }
     };
     (@str, $value:expr, $style:expr, $par_style:ident, $attr:ident) => {
         match $value {
@@ -227,7 +227,7 @@ macro_rules! apply {
                 $style.$attr = Some(value.clone());
             }
             _ => debug_assert!(false),
-        };
+        }
     };
 }
 
@@ -237,165 +237,59 @@ pub fn apply_properties(properties: &[Property], style: &mut Style, par_style: &
             Property::FontSize(_) => { /* already handled */ }
             Property::Color(_) => { /* already handled */ }
             Property::FontFamily(_) => { /* already handled */ }
-
-            Property::AlignContent(value) => {
-                apply!(@generic, value, style, par_style, align_content);
-            }
-            Property::AlignItems(value) => {
-                apply!(@generic, value, style, par_style, align_items);
-            }
-            Property::AlignSelf(value) => {
-                apply!(@generic, value, style, par_style, align_self);
-            }
-            Property::BackgroundColor(value) => {
-                apply!(@color, value, style, par_style, background_color);
-            }
+            Property::AlignContent(value) => apply!(@generic, value, style, par_style, align_content),
+            Property::AlignItems(value) => apply!(@generic, value, style, par_style, align_items),
+            Property::AlignSelf(value) => apply!(@generic, value, style, par_style, align_self),
+            Property::BackgroundColor(value) => apply!(@color, value, style, par_style, background_color),
             // TODO - for gradients
-            /*Property::BackgroundImage(_) => {
-                todo!();
-                //apply!(@generic_opt, value, arena[id].style, par_style, background_image);
-            }*/
-            Property::BorderBottomColor(value) => {
-                apply!(@color, value, style, par_style, border_bottom_color);
-            }
-            Property::BorderBottomLeftRadius(value) => {
-                apply!(@length, value, style, par_style, border_bottom_left_radius);
-            }
-            Property::BorderBottomRightRadius(value) => {
-                apply!(@length, value, style, par_style, border_bottom_right_radius);
-            }
-            Property::BorderBottomWidth(value) => {
-                apply!(@length, value, style, par_style, border_bottom_width);
-            }
-            Property::BorderLeftColor(value) => {
-                apply!(@color, value, style, par_style, border_left_color);
-            }
-            Property::BorderLeftWidth(value) => {
-                apply!(@length, value, style, par_style, border_left_width);
-            }
-            Property::BorderRightColor(value) => {
-                apply!(@color, value, style, par_style, border_right_color);
-            }
-            Property::BorderRightWidth(value) => {
-                apply!(@length, value, style, par_style, border_right_width);
-            }
-            Property::BorderTopColor(value) => {
-                apply!(@color, value, style, par_style, border_top_color);
-            }
-            Property::BorderTopLeftRadius(value) => {
-                apply!(@length, value, style, par_style, border_top_left_radius);
-            }
-            Property::BorderTopRightRadius(value) => {
-                apply!(@length, value, style, par_style, border_top_right_radius);
-            }
-            Property::BorderTopWidth(value) => {
-                apply!(@length, value, style, par_style, border_top_width);
-            }
-            Property::Bottom(value) => {
-                apply!(@length_opt, value, style, par_style, bottom);
-            }
-            Property::BoxShadowOffsetX(value) => {
-                apply!(@length, value, style, par_style, box_shadow_offset_x);
-            }
-            Property::BoxShadowOffsetY(value) => {
-                apply!(@length, value, style, par_style, box_shadow_offset_y);
-            }
-            Property::BoxShadowBlur(value) => {
-                apply!(@length, value, style, par_style, box_shadow_blur);
-            }
-            Property::BoxShadowColor(value) => {
-                apply!(@color, value, style, par_style, box_shadow_color);
-            }
-            Property::BoxShadowInset(value) => {
-                apply!(@generic_opt, value, style, par_style, box_shadow_inset);
-            }
-            Property::Cursor(value) => {
-                apply!(@generic, value, style, par_style, cursor);
-            }
-            Property::FlexBasis(value) => {
-                apply!(@length_opt, value, style, par_style, flex_basis);
-            }
-            Property::FlexDirection(value) => {
-                apply!(@generic, value, style, par_style, flex_direction);
-            }
-            Property::FlexGrow(value) => {
-                apply!(@generic, value, style, par_style, flex_grow);
-            }
-            Property::FlexShrink(value) => {
-                apply!(@generic, value, style, par_style, flex_shrink);
-            }
-            Property::FlexWrap(value) => {
-                apply!(@generic, value, style, par_style, flex_wrap);
-            }
-            Property::FontWeight(value) => {
-                apply!(@generic, value, style, par_style, font_weight);
-            }
-            Property::Height(value) => {
-                apply!(@length_opt, value, style, par_style, height);
-            }
-            Property::JustifyContent(value) => {
-                apply!(@generic, value, style, par_style, justify_content);
-            }
-            Property::Left(value) => {
-                apply!(@length_opt, value, style, par_style, left);
-            }
-            Property::MarginBottom(value) => {
-                apply!(@length_opt, value, style, par_style, margin_bottom);
-            }
-            Property::MarginLeft(value) => {
-                apply!(@length_opt, value, style, par_style, margin_left);
-            }
-            Property::MarginRight(value) => {
-                apply!(@length_opt, value, style, par_style, margin_right);
-            }
-            Property::MarginTop(value) => {
-                apply!(@length_opt, value, style, par_style, margin_top);
-            }
-            Property::MaxHeight(value) => {
-                apply!(@length_max, value, style, par_style, max_height);
-            }
-            Property::MaxWidth(value) => {
-                apply!(@length_max, value, style, par_style, max_width);
-            }
-            Property::MinHeight(value) => {
-                apply!(@length_min, value, style, par_style, min_height);
-            }
-            Property::MinWidth(value) => {
-                apply!(@length_min, value, style, par_style, min_width);
-            }
-            Property::Opacity(value) => {
-                apply!(@generic, value, style, par_style, opacity);
-            }
-            Property::Order(value) => {
-                apply!(@generic, value, style, par_style, order);
-            }
-            Property::PaddingBottom(value) => {
-                apply!(@length, value, style, par_style, padding_bottom);
-            }
-            Property::PaddingLeft(value) => {
-                apply!(@length, value, style, par_style, padding_left);
-            }
-            Property::PaddingRight(value) => {
-                apply!(@length, value, style, par_style, padding_right);
-            }
-            Property::PaddingTop(value) => {
-                apply!(@length, value, style, par_style, padding_top);
-            }
-            Property::Position(value) => {
-                apply!(@generic, value, style, par_style, position);
-            }
-            Property::Right(value) => {
-                apply!(@length_opt, value, style, par_style, right);
-            }
-            Property::Top(value) => {
-                apply!(@length_opt, value, style, par_style, top);
-            }
-            Property::Width(value) => {
-                apply!(@length_opt, value, style, par_style, width);
-            }
-            Property::ZIndex(value) => {
-                apply!(@generic, value, style, par_style, z_index);
-            }
+            /*Property::BackgroundImage(_) => apply!(@generic_opt, value, arena[id].style, par_style, background_image),*/
+            Property::BorderBottomColor(value) => apply!(@color, value, style, par_style, border_bottom_color),
+            Property::BorderBottomLeftRadius(value) => apply!(@length, value, style, par_style, border_bottom_left_radius),
+            Property::BorderBottomRightRadius(value) => apply!(@length, value, style, par_style, border_bottom_right_radius),
+            Property::BorderBottomWidth(value) => apply!(@length, value, style, par_style, border_bottom_width),
+            Property::BorderLeftColor(value) => apply!(@color, value, style, par_style, border_left_color),
+            Property::BorderLeftWidth(value) => apply!(@length, value, style, par_style, border_left_width),
+            Property::BorderRightColor(value) => apply!(@color, value, style, par_style, border_right_color),
+            Property::BorderRightWidth(value) => apply!(@length, value, style, par_style, border_right_width),
+            Property::BorderTopColor(value) => apply!(@color, value, style, par_style, border_top_color),
+            Property::BorderTopLeftRadius(value) => apply!(@length, value, style, par_style, border_top_left_radius),
+            Property::BorderTopRightRadius(value) => apply!(@length, value, style, par_style, border_top_right_radius),
+            Property::BorderTopWidth(value) => apply!(@length, value, style, par_style, border_top_width),
+            Property::Bottom(value) => apply!(@length_opt, value, style, par_style, bottom),
+            Property::BoxShadowOffsetX(value) => apply!(@length, value, style, par_style, box_shadow_offset_x),
+            Property::BoxShadowOffsetY(value) => apply!(@length, value, style, par_style, box_shadow_offset_y),
+            Property::BoxShadowBlur(value) => apply!(@length, value, style, par_style, box_shadow_blur),
+            Property::BoxShadowColor(value) => apply!(@color, value, style, par_style, box_shadow_color),
+            Property::BoxShadowInset(value) => apply!(@generic_opt, value, style, par_style, box_shadow_inset),
+            Property::Cursor(value) => apply!(@generic, value, style, par_style, cursor),
+            Property::FlexBasis(value) => apply!(@length_opt, value, style, par_style, flex_basis),
+            Property::FlexDirection(value) => apply!(@generic, value, style, par_style, flex_direction),
+            Property::FlexGrow(value) => apply!(@generic, value, style, par_style, flex_grow),
+            Property::FlexShrink(value) => apply!(@generic, value, style, par_style, flex_shrink),
+            Property::FlexWrap(value) => apply!(@generic, value, style, par_style, flex_wrap),
+            Property::FontWeight(value) => apply!(@generic, value, style, par_style, font_weight),
+            Property::Height(value) => apply!(@length_opt, value, style, par_style, height),
+            Property::JustifyContent(value) => apply!(@generic, value, style, par_style, justify_content),
+            Property::Left(value) => apply!(@length_opt, value, style, par_style, left),
+            Property::MarginBottom(value) => apply!(@length_opt, value, style, par_style, margin_bottom),
+            Property::MarginLeft(value) => apply!(@length_opt, value, style, par_style, margin_left),
+            Property::MarginRight(value) => apply!(@length_opt, value, style, par_style, margin_right),
+            Property::MarginTop(value) => apply!(@length_opt, value, style, par_style, margin_top),
+            Property::MaxHeight(value) => apply!(@length_max, value, style, par_style, max_height),
+            Property::MaxWidth(value) => apply!(@length_max, value, style, par_style, max_width),
+            Property::MinHeight(value) => apply!(@length_min, value, style, par_style, min_height),
+            Property::MinWidth(value) => apply!(@length_min, value, style, par_style, min_width),
+            Property::Opacity(value) => apply!(@generic, value, style, par_style, opacity),
+            Property::Order(value) => apply!(@generic, value, style, par_style, order),
+            Property::PaddingBottom(value) => apply!(@length, value, style, par_style, padding_bottom),
+            Property::PaddingLeft(value) => apply!(@length, value, style, par_style, padding_left),
+            Property::PaddingRight(value) => apply!(@length, value, style, par_style, padding_right),
+            Property::PaddingTop(value) => apply!(@length, value, style, par_style, padding_top),
+            Property::Position(value) => apply!(@generic, value, style, par_style, position),
+            Property::Right(value) => apply!(@length_opt, value, style, par_style, right),
+            Property::Top(value) => apply!(@length_opt, value, style, par_style, top),
+            Property::Width(value) => apply!(@length_opt, value, style, par_style, width),
+            Property::ZIndex(value) => apply!(@generic, value, style, par_style, z_index),
         }
     }
 }
